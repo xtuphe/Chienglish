953427dcd3299a83e31476c17700158930d402ab
Xtuphe <xtuphe@live.com>
2019-06-20 19:54:53 +0800

    引入OC类似AppStore框架

--- Chienglish/AppDelegate.swift
+++ Chienglish/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow.init(frame: UIScreen.main.bounds)
+        window!.backgroundColor = UIColor.white
+        window!.rootViewController = TabBarController.init(nibName: "TabBarController", bundle: nil)
+        window!.makeKeyAndVisible()
         return true
     }
 
